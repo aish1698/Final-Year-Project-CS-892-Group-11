593ebe4d11942b81998d0fffd354ea76444507a2
banerjeeo <53657474+banerjeeo@users.noreply.github.com>
2021-04-11 19:47:25 +0530

    final

--- FinalYearProject/FinalYearProject/FinalYearProject/NewE-classroom/front-end/src/app/app.module.ts
+++ FinalYearProject/FinalYearProject/FinalYearProject/NewE-classroom/front-end/src/app/app.module.ts
@@ -19,8 +19,7 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import{ForgotpasswordteacherComponent} from './forgotpasswordteacher/forgotpasswordteacher.component'
 import { loginteacherComponent } from './user/loginteacher/loginteacher.component';
-import { AllProfilesComponent } from './components/all-profiles/all-profiles.component';
-import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+
 import { teacherregistrationComponent } from './teacherregistration/teacherregistration.component';
 import { SignupComponent } from './signup/signup.component';
 import { TeacherComponent } from './teacher/teacher.component';
@@ -46,8 +45,7 @@ import { AnswerdownloadComponent } from './answerdownload/answerdownload.compone
     HomeComponent,
 
 
-    AllProfilesComponent,
-    CreateProfileComponent,
+  
     teacherregistrationComponent,
     SignupComponent,
    TeacherComponent,
